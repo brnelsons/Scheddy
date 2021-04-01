681a21be72895976f5585ed6b67db13596a60806
bnelson <brnelsons139@gmail.com>
2021-03-31 20:19:46 -0500

    LS-4 Fixed lint errors

--- src/app/core/components/time-range-selector/time-range-selector.component.ts
+++ src/app/core/components/time-range-selector/time-range-selector.component.ts
@@ -28,24 +28,23 @@ export class TimeRangeSelectorComponent implements OnInit {
     for (let hour = 7; hour < 21; hour++) {
       for (let minuteStep = 0; minuteStep < 60; minuteStep += 30) {
         const formattedMinute = minuteStep === 0 ? "00" : minuteStep;
+
         this.times.push(
           {
-            time: hour + ":" + formattedMinute,
-            display: hour <= 12
-              ? hour + ":" + formattedMinute + " AM"
-              : hour - 12 + ":" + formattedMinute + " PM"
+            time: `${hour}:${formattedMinute}`,
+            display: `${hour <= 12 ? hour : hour - 12}:${formattedMinute} ${hour <= 12 ? 'AM' : 'PM'}`,
           }
-        )
+        );
       }
     }
   }
 
-  calculate() {
+  calculate(): void {
     if (this.timeRange.endTime === "" && this.timeRange.startTime !== null && this.timeRange.startTime !== "") {
       // update end time to be starttime + 8 hours;
       const startSplit = this.timeRange.startTime.split(":");
-      this.timeRange.endTime = "" + (Number.parseInt(startSplit[0]) + 8) + ":" + startSplit[1];
-      this.timeRangeChange.emit(this.timeRange)
+      this.timeRange.endTime = `${Math.min(Number.parseInt(startSplit[0]) + 8, 20)}:${startSplit[1]}`;
+      this.timeRangeChange.emit(this.timeRange);
     }
     this.employee.calculate();
   }
